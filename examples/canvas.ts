32e74cc5c1e1cedc19b78cd665e26c489f4c0e6d
Isaiah Gardeen <isaiah@gardeen.net>
2024-11-01 17:52:02 -0500

    some stuff initial kinda

---
+++ examples/canvas.ts
@@ -0,0 +1,21 @@
+import { mainloop, WindowCanvas } from "https://deno.land/x/dwm@0.3.4/ext/canvas.ts";
+
+const win = new WindowCanvas({
+  title: "Skia Canvas",
+  width: 800,
+  height: 600,
+  resizable: true,
+});
+
+win.onDraw = (ctx) => {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "white";
+  ctx.font = "30px Arial";
+  ctx.textBaseline = "top";
+  ctx.fillText("Hello World", 10, 10);
+};
+
+await mainloop(() => {
+  win.draw();
+});
