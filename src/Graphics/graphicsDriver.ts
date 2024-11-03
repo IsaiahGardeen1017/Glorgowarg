c5e754d616ab9c3b0b6f814df9ccea2e9cd9d17d
Isaiah Gardeen <isaiah@gardeen.net>
2024-11-03 15:03:48 -0600

    Got basic scaling working

--- src/Graphics/graphicsDriver.ts
+++ src/Graphics/graphicsDriver.ts
@@ -7,7 +7,7 @@ import * as gl from "https://deno.land/x/gluten@0.1.3/api/gles23.2.ts";
 import { GameState } from "../simulation/GameState.ts";
 import { GLRGB, HexToGLRGB, HexToRGB} from "./colorUtils.ts";
 import { combineShapes, generateSquareCentered } from "./shapeFunctions.ts";
-import { loadShader } from "./OpenGLFunctions.ts";
+import { loadShader } from "./glFunctions.ts";
 
 export async function startOpenGlWindow(game: GameState) {
     const window = createWindow({
@@ -30,7 +30,7 @@ export async function startOpenGlWindow(game: GameState) {
     out vec4 color;
 
     void main() {
-      gl_Position = vPosition;
+      gl_Position = vPosition * vScalar;
       color = vColor;
     }
     `;
@@ -48,18 +48,27 @@ export async function startOpenGlWindow(game: GameState) {
     const vShader = loadShader(gl.VERTEX_SHADER, vShaderSrc);
     const fShader = loadShader(gl.FRAGMENT_SHADER, fShaderSrc);
 
-    const program = gl.CreateProgram();
+    const program:gl.GLuint = gl.CreateProgram();
     gl.AttachShader(program, vShader);
     gl.AttachShader(program, fShader);
 
-    const vPosAttInt = 0;
-    const vColAttInt = 1;
-    const vScalarAttInt = 2;
-    gl.BindAttribLocation(program, vPosAttInt, new TextEncoder().encode("vPosition\0"));
-    gl.BindAttribLocation(program, vColAttInt, new TextEncoder().encode("vColor\0"));
-    gl.BindAttribLocation(program, vScalarAttInt, new TextEncoder().encode("vScalar\0"));
-
     gl.LinkProgram(program);
+
+    const vPosAttInt = gl.GetAttribLocation(program, new TextEncoder().encode("vPosition\0"));
+    const vColAttInt = gl.GetAttribLocation(program, new TextEncoder().encode("vColor\0"));
+    const vScalarAttInt = gl.GetUniformLocation(program, new TextEncoder().encode("vScalar\0"));
+
+    console.log(vPosAttInt);
+    console.log(vColAttInt);
+    console.log(vScalarAttInt);
+
+    //gl.BindAttribLocation(program, vPosAttInt, new TextEncoder().encode("vPosition\0"));
+    //gl.BindAttribLocation(program, vColAttInt, new TextEncoder().encode("vColor\0"));
+    //gl.BindAttribLocation(program, vScalarAttInt, new TextEncoder().encode("vScalar\0"));
+
+    
+
+    
 
     const status = new Int32Array(1);
     gl.GetProgramiv(program, gl.LINK_STATUS, status);
@@ -80,8 +89,29 @@ export async function startOpenGlWindow(game: GameState) {
         gl.Viewport(0, 0, event.width, event.height);
     });
 
+    let zoomLevel = 100;
+    addEventListener("keydown", (evt) => {
+        if (evt.code === "KeyW") {
+            //Zoom Out
+            if(zoomLevel > 5){
+                zoomLevel-=5;
+            }
+        } else if (evt.code === "KeyS") {
+            //Zoome In
+            zoomLevel+=5;
+        }
+    });
+
+    const BASE_SCALE = 1000.0;
+
     function frame() {
-        const scalerVector = [2.0,1.0,1.0,1.0];
+        const viewport = new Int32Array(4);
+        gl.GetIntegerv(gl.VIEWPORT, viewport);
+        const width = viewport[2];
+        const height = viewport[3];
+
+        const scale = BASE_SCALE * (zoomLevel / 100.0);
+        const scalerVector = [scale/width,scale/height,1.0,1.0];
 
         gl.Clear(gl.COLOR_BUFFER_BIT);
         gl.UseProgram(program);
@@ -96,6 +126,8 @@ export async function startOpenGlWindow(game: GameState) {
 
         gl.VertexAttribPointer(vColAttInt, 3, gl.FLOAT, gl.FALSE, 0, renderBuffer.colors);
         gl.EnableVertexAttribArray(vColAttInt);
+
+        gl.Uniform4f(vScalarAttInt, scalerVector[0], scalerVector[1], scalerVector[2], scalerVector[3]);
 
         gl.DrawArrays(gl.TRIANGLES, 0, renderBuffer.numTriangles);
         window.swapBuffers();
--- src/Graphics/shaders/2d-simple-vertex.glsl
+++ src/Graphics/shaders/2d-simple-vertex.glsl
@@ -1,8 +1,11 @@
-#version 300 es
-in vec4 vPosition;
-in vec4 vColor;
-out vec4 color;
-void main() {
-  gl_Position = vPosition;
-  color = vColor;
-}
+    #version 300 es
+    in vec4 vPosition;
+    in vec4 vColor;
+    uniform vec4 vScalar;
+
+    out vec4 color;
+
+    void main() {
+      gl_Position = vec4(vPosition.x * vScalar.x, vPosition.y * vScalar.y, vPosition.z * vScalar.z, 1.0);
+      color = vColor;
+    }
