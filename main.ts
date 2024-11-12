95b07da5c973611668acddce12aa0d0a2e697f2a
Isaiah Gardeen <isaiah@gardeen.net>
2024-11-11 22:05:11 -0600

    webgpu start

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
 import { startCanvasWindow } from "./src/canvasing/canvasDriver.ts";
 import { drawMap } from "./src/canvasing/drawMap.ts";
 import { startOpenGlWindow } from "./src/Graphics/graphicsDriver.ts";
+import { startWebGpuWindow } from "./src/Graphics/webGPUgraphicsDriver.ts";
 import { startHeadlessSimulation } from "./src/Headless/driver.ts";
 import { logTiming } from "./src/loggingFuncs.ts";
 import { GameState } from "./src/simulation/GameState.ts";
 
-const WIDTH = 1000;
-const HEIGHT =700;
+const WIDTH = 200;
+const HEIGHT =100;
 logTiming('Glorowarg Started');
 const gameState = new GameState(WIDTH, HEIGHT);
 logTiming('GameState set');
 
 //startCanvasWindow(gameState, drawMap);
 
-startOpenGlWindow(gameState);
+//startOpenGlWindow(gameState);
+startWebGpuWindow(gameState);
+
 
 //startHeadlessSimulation(gameState);
--- src/Graphics/graphicsDriver.ts
+++ src/Graphics/graphicsDriver.ts
@@ -18,15 +18,13 @@ import { loadShader } from "./glFunctions.ts";
 import { logTiming } from "../loggingFuncs.ts";
 import { GLFW_ACCUM_ALPHA_BITS } from "https://deno.land/x/dwm@0.3.7/src/platform/glfw/constants.ts";
 
-
-
 const BASE_SCALE = 1000;
 
 const START_VIEWPORT_WIDTH = Math.floor(BASE_SCALE) * 2;
 const START_VIEWPORT_HEIGHT = Math.floor(BASE_SCALE);
 
 export async function startOpenGlWindow(game: GameState) {
-    let timeCounter = logTiming('Starting graphics driver');
+    let timeCounter = logTiming("Starting graphics driver");
     const window = createWindow({
         title: "DenoGL",
         width: START_VIEWPORT_WIDTH,
@@ -35,9 +33,8 @@ export async function startOpenGlWindow(game: GameState) {
         glVersion: "v3.2",
         gles: true,
     });
-    
-    timeCounter = logTiming('Created Window');
-    
+
+    timeCounter = logTiming("Created Window");
 
     gl.load(getProcAddress);
 
@@ -77,11 +74,10 @@ export async function startOpenGlWindow(game: GameState) {
     gl.LinkProgram(program);
 
     //Get shader input integers
-    const staticAttrbLoc_vPosition = gl.GetAttribLocation(program,new TextEncoder().encode("vPosition\0"),);
-    const staticAttrbLoc_vColor = gl.GetAttribLocation(program,new TextEncoder().encode("vColor\0"),);
-    const unifLoc_vScalar = gl.GetUniformLocation(program,new TextEncoder().encode("vScalar\0"),);
-    const unifLoc_vPan = gl.GetUniformLocation(program,new TextEncoder().encode("vPan\0"),);
-
+    //const staticAttrbLoc_vPosition = gl.GetAttribLocation(program,new TextEncoder().encode("vPosition\0"),);
+    //const staticAttrbLoc_vColor = gl.GetAttribLocation(program,new TextEncoder().encode("vColor\0"),);
+    //const unifLoc_vScalar = gl.GetUniformLocation(program,new TextEncoder().encode("vScalar\0"),);
+    //const unifLoc_vPan = gl.GetUniformLocation(program,new TextEncoder().encode("vPan\0"),);
 
     //Do some stuff (?)
     const status = new Int32Array(1);
@@ -95,7 +91,6 @@ export async function startOpenGlWindow(game: GameState) {
         gl.DeleteProgram(program);
         Deno.exit(1);
     }
-
 
     gl.UseProgram(program);
 
@@ -115,7 +110,7 @@ export async function startOpenGlWindow(game: GameState) {
         const scrollValue = evt.scrollY; // 1 or -1
         const zoomDelta = zoomLevel * 0.1 * scrollValue;
         zoomLevel += zoomDelta;
-        
+
         if (zoomLevel >= maxZoom) {
             zoomLevel = maxZoom;
         } else if (zoomLevel <= minZoom) {
@@ -125,56 +120,61 @@ export async function startOpenGlWindow(game: GameState) {
 
     addEventListener("keydown", (evt) => {
         console.log(evt.code);
-        console.log('zoom:' + zoomLevel);
+        console.log("zoom:" + zoomLevel);
         const mem = Deno.memoryUsage();
-        console.log('rss: ' + mem.rss * 0.000001);
-        console.log('heapTotal: ' + mem.heapTotal * 0.000001);
-        console.log('heapUsed: ' + mem.heapUsed * 0.000001);
-        console.log('external: '  + mem.external * 0.000001);
+        console.log("rss: " + mem.rss * 0.000001);
+        console.log("heapTotal: " + mem.heapTotal * 0.000001);
+        console.log("heapUsed: " + mem.heapUsed * 0.000001);
+        console.log("external: " + mem.external * 0.000001);
     });
 
     let panX = game.map.xSize / -2.0;
     let panY = game.map.ySize / -2.0;
 
-
-    logTiming('GL setup complete');
+    logTiming("GL setup complete");
 
     let tileShapes = getTileShapes(game);
     let tileShapesData = combineShapes(tileShapes);
     const numTileTriangles = tileShapesData.numTriangles;
 
-    
-    
-    logTiming('Shapes arrays compiled');
-    
+    logTiming("Shapes arrays compiled");
 
-    gl.VertexAttribPointer(staticAttrbLoc_vPosition,3,gl.FLOAT,gl.FALSE,0,tileShapesData.vertices);
-    gl.EnableVertexAttribArray(staticAttrbLoc_vPosition);
-    
-    gl.VertexAttribPointer(staticAttrbLoc_vColor,3,gl.FLOAT,gl.FALSE,0,tileShapesData.colors);
-    gl.EnableVertexAttribArray(staticAttrbLoc_vColor);
-    
-    logTiming('Attributes set, starting loop');
-    
+    console.log("HERHE");
+    let buff: gl.Buffer = new Float32Array();
+    console.log(buff);
+    gl.GenBuffers(1, buff);
+    console.log(buff);
+
+    gl.BindBuffer(gl.ARRAY_BUFFER, 0);
+
+    console.log("idk");
+    gl.GenBuffers(1, tileShapesData.colors);
+    //gl.VertexAttribPointer(staticAttrbLoc_vPosition,3,gl.FLOAT,gl.FALSE,0,tileShapesData.vertices);
+    //gl.EnableVertexAttribArray(staticAttrbLoc_vPosition);
+
+    //gl.VertexAttribPointer(staticAttrbLoc_vColor,3,gl.FLOAT,gl.FALSE,0,tileShapesData.colors);
+    //gl.EnableVertexAttribArray(staticAttrbLoc_vColor);
+
+    logTiming("Attributes set, starting loop");
+
     //Do every frame
     function frame() {
         game.process();
-        
+
         const viewport = new Int32Array(4);
         gl.GetIntegerv(gl.VIEWPORT, viewport);
         const width = viewport[2];
         const height = viewport[3];
-        
+
         const scale = BASE_SCALE * (zoomLevel / 100.0);
         const scalerVector = [scale / width, scale / height, 1.0, 1.0];
         const panVector = [panX, panY, 0.0, 0.0]; // Since we add this to the vertex position vector we want the last two to be 0.0
-        
-        
+
         gl.Clear(gl.COLOR_BUFFER_BIT);
-        
+
         //Set Uniforms
-        gl.Uniform4f(unifLoc_vScalar,scalerVector[0],scalerVector[1],scalerVector[2],scalerVector[3],);
-        gl.Uniform4f(unifLoc_vPan,panVector[0],panVector[1],panVector[2],panVector[3],);
+        //gl.Uniform4f(unifLoc_vScalar,scalerVector[0],scalerVector[1],scalerVector[2],scalerVector[3],);
+        //gl.Uniform4f(unifLoc_vPan,panVector[0],panVector[1],panVector[2],panVector[3],);
 
         gl.DrawArrays(gl.TRIANGLES, 0, numTileTriangles);
         window.swapBuffers();
@@ -182,5 +182,3 @@ export async function startOpenGlWindow(game: GameState) {
 
     await mainloop(frame);
 }
-
-
