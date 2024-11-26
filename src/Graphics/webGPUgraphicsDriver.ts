778d67aca0ecf7a30e0030a912be0c97913d0d73
Isaiah Gardeen <igardeen@meditech.com>
2024-11-25 21:44:13 -0600

    working very very close

--- src/Graphics/webGPUgraphicsDriver.ts
+++ src/Graphics/webGPUgraphicsDriver.ts
@@ -55,16 +55,22 @@ export async function startWebGpuWindow(game: GameState) {
         label: 'Cell shader',
         code: `
 
+        struct VertexOutput {
+            @builtin(position) pos: vec4f,
+            @location(0) color: vec4f,
+        };
 
         @vertex
-        fn vertexMain(@location(0) pos: vec2f) ->
-            @builtin(position) vec4f {
-            return vec4f(pos, 0, 1);
+        fn vertexMain(@location(0) pos: vec2f) -> VertexOutput {
+            var output: VertexOutput;
+            output.pos = vec4f(pos, 0, 1);
+            output.color = vec4f(0.8, 0.2, 0.2, 1.0);
+            return output;
         }
       
         @fragment
-        fn fragmentMain() -> @location(0) vec4f {
-            return vec4f(0, 1, 0, 1);
+        fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
+            return vec4f(input.color);
         }
         `
     });
